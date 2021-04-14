35376f28395b177248105ca1d484b05a164070f1
wilfried <wilpel90@gmail.com>
2021-04-14 14:52:59 +0200

    ajout d'image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
 
 const Home = () => {
     return(
         <div className="home">
             <Navigation />
+            <Logo />
             <h1>Accueil</h1>
             
         </div>
