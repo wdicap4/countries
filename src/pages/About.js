6f633f0fd8ae86ae1fc07e7ae36ad459c39b90c5
wilfried <wilpel90@gmail.com>
2021-04-14 14:36:22 +0200

    configuration de base

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Les pages À Propos ne sont souvent pas la priorité des e-commerçants —
+        après tout, il ne s’agit que d’un lien qui restera enfoui dans le pied
+        de page du site et qui redirigera vers quelques paragraphes rédigés à la
+        hâte au sujet de l’entreprise. Mais, une page À Propos efficace devrait
+        en réalité être une page de vente qui dispose d’objectifs bien définis.
+        C’est une page qui devrait mettre en avant les points forts de votre
+        histoire et de votre marque, et laisser une impression durable dans
+        l’esprit des clients curieux.
+      </p>
+      <p><br/><br/><br/>
+        Les pages À Propos ne sont souvent pas la priorité des e-commerçants —
+        après tout, il ne s’agit que d’un lien qui restera enfoui dans le pied
+        de page du site et qui redirigera vers quelques paragraphes rédigés à la
+        hâte au sujet de l’entreprise. Mais, une page À Propos efficace devrait
+        en réalité être une page de vente qui dispose d’objectifs bien définis.
+        C’est une page qui devrait mettre en avant les points forts de votre
+        histoire et de votre marque, et laisser une impression durable dans
+        l’esprit des clients curieux.
+      </p>
+    </div>
+  );
+};
+
+export default About;
