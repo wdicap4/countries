ced4f2b50669a94d7adad1837c3144e7fc8cefe3
wilfried <wilpel90@gmail.com>
2021-04-14 17:53:50 +0200

    on retourne les données de l'api (nom du pays, capital,population drapeau)

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Cards = (props) => {
     const { country } =props;
-    console.log(country);
+
     return (
-        <div>
-            card;
-        </div>
+      <li>
+          <img src={country.flag} alt="flag" />
+          <div className="data-container">
+              <ul>
+                  <li>{country.name}</li>
+                  <li>{country.capital}</li>
+                  <li>{country.population}</li>
+              </ul>
+
+          </div>
+      </li>
     );
 };
 
