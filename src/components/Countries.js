1d50d354d8b064073444313743cf28687750f96f
wilfried <wilpel90@gmail.com>
2021-04-14 17:30:32 +0200

    hoock useState&UseEffect; API&Axios

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Countries = () => {
-    return (
-        <div>
-            countries
-        </div>
-    );
+  const [data, setData] = useState([]);
+  
+  useEffect(() =>{
+  axios
+    .get(
+      "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+    )
+    .then((res) => setData(res.data));
+    console.log(data);
+  }, []);
+
+  return (
+     <div className="countries">
+       <ul className="countries-list">
+         {data.map((country) =>(
+           <li>{country.name}</li>
+         ))}
+       </ul>
+     </div>
+  );
 };
 
 export default Countries;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
             <Navigation />
             <Logo />
             <Countries />
-            <h1>Accueil</h1>
             
         </div>
     );
