b928534bf251921945a1ae2f3dbaa6a0fe81c454
wilfried <wilpel90@gmail.com>
2021-04-14 18:25:36 +0200

    ajout d'une regExp pour séparrer les caractères

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Card = (props) => {
     const { country } =props;
 
+    const numberFormat = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
     return (
       <li className ="card">
           <img src={country.flag} alt="flag" />
